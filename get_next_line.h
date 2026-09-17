b3098f29bfd31b70178132d0b431efb4ef888ce7
ishtiaqjlm <ishtiaqjlm@gmail.com>
2026-09-17 02:23:45 +0200

    17 sep 2026 02:23

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ishtiahm <ishtiahm@student.42prague.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/17 01:26:15 by ishtiahm          #+#    #+#             */
+/*   Updated: 2026/09/17 01:26:15 by ishtiahm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+
+#endif
